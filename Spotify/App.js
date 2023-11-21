2dfb19ec1811094a686e5ddb3ae99639023e0f8f
Smile005 <haducanh1802@gmail.com>
2023-11-21 17:46:42 +0700

    update StackNavigation.js

--- Spotify/App.js
+++ Spotify/App.js
@@ -1,17 +1,12 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import DangNhap from './screens/DangNhap'
-import TrangChu from './screens/HomeScreen'
+import Navigation from "./StackNavigation"
 
 export default function App() {
-  const Stack = createNativeStackNavigator()
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown:true}}>
-        <Stack.Screen component={DangNhap} name='DangNhap'/>
-        {/* <Stack.Screen component={TrangChu} name='TrangChu'/> */}
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <Navigation />
+    </>
   );
 }
+
 
--- Spotify/screens/HomeScreen.js
+++ Spotify/screens/HomeScreen.js
@@ -4,7 +4,7 @@ import React from 'react'
 const HomeScreen = () => {
   return (
     <View>
-      <Text>ProfileScreen</Text>
+      <Text>HomeScreen</Text>
     </View>
   )
 }
